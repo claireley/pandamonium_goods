4964b9e5ba81037a7e29e06774ec35da6fce20a0
claireley <claire.leyden10@gmail.com>
2026-05-26 10:42:04 +0200

    First Claire's commit

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -5,22 +5,22 @@
 
 # Remember to modify your functions to use the template shown below
 
-def function_name(input1: data_type1, input2: data_type2,..., opt_arg: data_type_opt= default_value) -> output_data_type:
-	"""
-	Add a description of what the function does
+#def function_name(input1: data_type1, input2: data_type2,..., opt_arg: data_type_opt= default_value) -> output_data_type:#
+	# """
+	# Add a description of what the function does
 
-	Arguments:
-	---------
-	input1: data_type and which information should contain
-	input2: data_type and which information should contain
-	opt_arg: data_type and which information should contain
+	# Arguments:
+	# ---------
+	# input1: data_type and which information should contain
+	# input2: data_type and which information should contain
+	# opt_arg: data_type and which information should contain
 
-	Outputs:
-	-------
-	data_type and which information will contain
-	"""
+	# Outputs:
+	# -------
+	# data_type and which information will contain
+	# """
 	
-	# Your function code here
+	# # Your function code here
 
 	
-	return opuput
+	# return opuput
